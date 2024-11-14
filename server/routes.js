c8f56185c3a9395aa758fc3312a1176106c554ea
Jason LaGrasse <jasonlagrasse@gmail.com>
2024-11-13 22:47:08 -0500

    FIX: Bcrypt compatibility

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 const User = require('./model.js');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 module.exports = (app) => {
