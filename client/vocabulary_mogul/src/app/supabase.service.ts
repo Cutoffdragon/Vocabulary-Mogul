da0b4e2a3119e7fa86a6c27525040abe2a270c48
Jason LaGrasse <jasonlagrasse@gmail.com>
2025-07-21 17:10:15 -0400

    FIX: Supabase connectivity

--- client/vocabulary_mogul/src/app/supabase.service.ts
+++ client/vocabulary_mogul/src/app/supabase.service.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 import { VocabularyDefinition } from './vocab-definition';
 import { environment } from './environments/environment';
 
-const supabaseUrl = environment.supabaseURL;
-const supabaseKey = environment.supabaseKey;
+const supabaseUrl = 'https://fjvswnxotlbfadmatuae.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqdnN3bnhvdGxiZmFkbWF0dWFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4NzYyNzEsImV4cCI6MjA2ODQ1MjI3MX0.LDzj5xxjuXWa2POCm8cIRBBKMhsPmi_iBgJluMUHiq4';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 @Injectable({
@@ -40,6 +40,7 @@ export class SupabaseService {
   }
 
   async getDefinitionsById(idArray: number[]): Promise<string[]> {
+
     const definitionArray: string[] = []
     for (let i = 0; i < idArray.length; i++) {
         const { data, error } = await supabase
