76d15ed02f7e2a2609d4e637cb9f0e3897ec8725
Jason LaGrasse <jasonlagrasse@gmail.com>
2025-07-21 16:28:07 -0400

    FIX: Supabase configuration

--- client/vocabulary_mogul/src/app/environments/environment.prod.ts
+++ client/vocabulary_mogul/src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: true,
-    apiURL: 'API_URL',
-    supabaseURL: 'SUPABASE_URL',
-    supabaseKey: 'SUPABASE_KEY'
-  };
+  production: true,
+  apiURL: "https://vocabulary-mogulserver-production.up.railway.app",
+  supabaseURL: "https://fjvswnxotlbfadmatuae.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqdnN3bnhvdGxiZmFkbWF0dWFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4NzYyNzEsImV4cCI6MjA2ODQ1MjI3MX0.LDzj5xxjuXWa2POCm8cIRBBKMhsPmi_iBgJluMUHiq4"
+};
--- client/vocabulary_mogul/src/app/environments/environment.ts
+++ client/vocabulary_mogul/src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  apiURL: 'API_URL',
-  supabaseURL: 'SUPABASE_URL',
-  supabaseKey: 'SUPABASE_KEY'
+  apiURL: "https://vocabulary-mogulserver-production.up.railway.app",
+  supabaseURL: "https://fjvswnxotlbfadmatuae.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqdnN3bnhvdGxiZmFkbWF0dWFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4NzYyNzEsImV4cCI6MjA2ODQ1MjI3MX0.LDzj5xxjuXWa2POCm8cIRBBKMhsPmi_iBgJluMUHiq4"
 }
